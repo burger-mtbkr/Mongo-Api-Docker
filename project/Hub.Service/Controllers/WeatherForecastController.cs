811b1bf5a473e657dc2f3b2b5cd09c103983adab
Lóan Burger <loan.burger@xero.com>
2021-08-25 11:48:20 +1200

    Dockerize api

--- project/Hub.Service/Controllers/WeatherForecastController.cs
+++ project/Hub.Service/Controllers/WeatherForecastController.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hub.Service.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Hub.Service.Controllers
 {
+    [Route("api/[controller]")]
 	[ApiController]
-	[Route("[controller]")]
 	public class WeatherForecastController : ControllerBase
 	{
 		private static readonly string[] Summaries = new[]
